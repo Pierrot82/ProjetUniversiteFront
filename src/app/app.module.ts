69b765a9660099003199199040a65a848f59bb2e
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-07 14:17:55 +0200

    ajout Etudiant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormProfesseurComponent } from './Professeur/form-professeur/form-professeur.component';
-import { UpdateProfesseurComponent } from './Professeur/update-professeur/update-professeur.component';
-import { ListeProfesseurComponent } from './Professeur/liste-professeur/liste-professeur.component';
 import { FormEnseignantComponent } from './Enseignant/form-enseignant/form-enseignant.component';
 import { UpdateEnseignantComponent } from './Enseignant/update-enseignant/update-enseignant.component';
 import { ListeEnseignantComponent } from './Enseignant/liste-enseignant/liste-enseignant.component';
+import { EtudiantComponent } from './Etudiant/etudiant/etudiant.component';
+import { UpdateEtudiantComponent } from './Etudiant/update-etudiant/update-etudiant.component';
+import { ListeEtudiantComponent } from './Etudiant/liste-etudiant/liste-etudiant.component';
+import { FormEtudiantComponent } from './Etudiant/form-etudiant/form-etudiant.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormProfesseurComponent,
-    UpdateProfesseurComponent,
-    ListeProfesseurComponent,
     FormEnseignantComponent,
     UpdateEnseignantComponent,
-    ListeEnseignantComponent
+    ListeEnseignantComponent,
+    EtudiantComponent,
+    UpdateEtudiantComponent,
+    ListeEtudiantComponent,
+    FormEtudiantComponent
   ],
   imports: [
     BrowserModule,
