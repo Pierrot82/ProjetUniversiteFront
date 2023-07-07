427af4c6bb5c3758f37ce6330944623156db3e7f
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-07 16:59:31 +0200

    ajout formulaire etudiant, css associé et update des imports et path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormEnseignantComponent } from './Enseignant/form-enseignant/form-enseignant.component';
 import { ListeEnseignantComponent } from './Enseignant/liste-enseignant/liste-enseignant.component';
+import { ListeEtudiantComponent } from './Etudiant/liste-etudiant/liste-etudiant.component';
+import { FormEtudiantComponent } from './Etudiant/form-etudiant/form-etudiant.component';
 
 const routes: Routes = [
   { path: 'ajouterEnseignant', component: FormEnseignantComponent },
   
   { path: 'listeEnseignant', component: ListeEnseignantComponent },
+
+  { path: 'ajoutEtudiant', component: FormEtudiantComponent },
  
+  { path: 'getListeEtudiant', component: ListeEtudiantComponent },
 ];
 
 @NgModule({
