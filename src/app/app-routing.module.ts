1ab47cf37855082d92c8c64d54e84ceee5bf09c0
dsoz34 <sofiane.antibes@gmail.com>
2023-07-07 14:00:22 +0200

    comit  : enseignant v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormEnseignantComponent } from './Enseignant/form-enseignant/form-enseignant.component';
+import { ListeEnseignantComponent } from './Enseignant/liste-enseignant/liste-enseignant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ajouterEnseignant', component: FormEnseignantComponent },
+  
+  { path: 'listeEnseignant', component: ListeEnseignantComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
