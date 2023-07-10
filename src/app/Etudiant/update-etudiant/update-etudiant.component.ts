6e55405904c6223940e408097bc4f36a7410e04e
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-10 14:40:32 +0200

    ajout css pour formulaires etudiants + correction boutons reset

--- src/app/Etudiant/update-etudiant/update-etudiant.component.ts
+++ src/app/Etudiant/update-etudiant/update-etudiant.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-etudiant',
   templateUrl: './update-etudiant.component.html',
   styleUrls: ['./update-etudiant.component.css']
 })
-export class UpdateEtudiantComponent {
+export class UpdateEtudiantComponent implements OnInit {
+  updateEtudiant!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.updateEtudiant = this.formBuilder.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      dateNaissance: ['', Validators.required],
+      dateInscription: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.updateEtudiant.valid) {
+      // Récupérer les valeurs du formulaire
+      const formData = this.updateEtudiant.value;
+      // Effectuer les opérations de mise à jour de l'étudiant
+      // ...
+    }
+  }
 }
+
+
