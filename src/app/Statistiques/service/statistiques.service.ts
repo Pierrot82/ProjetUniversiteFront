d19984b84e17f7cb525549ef9bde1e1580b9dba4
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-13 18:24:22 +0200

    FINAL statistiques + gestion conflits listecours

--- src/app/Statistiques/service/statistiques.service.ts
+++ src/app/Statistiques/service/statistiques.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Statistiques } from 'src/app/model/statistiques.model';
 
 @Injectable({
   providedIn: 'root'
--- src/app/Statistiques/statistiques.component.ts
+++ src/app/Statistiques/statistiques.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StatistiquesService } from './service/statistiques.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Statistiques } from '../model/statistiques.model';
 
 @Component({
   selector: 'app-statistiques',
@@ -12,6 +10,13 @@ import { Statistiques } from '../model/statistiques.model';
 export class StatistiquesComponent implements OnInit {
 
   moyenne!:number;
+  notelaplusbasse!:number;
+  notelaplushaute!:number;
+  nombreetu!:number;
+  nombrenseignants!:number;
+  nombrepostulants!:number;
+  jeanmich!:number;
+  totalmessages!:number;
 
   constructor(private statistiquesService: StatistiquesService, private http: HttpClient) { }
 
@@ -19,8 +24,26 @@ export class StatistiquesComponent implements OnInit {
     this.http.get<number>("http://localhost:8080/moyenne_notes").subscribe(data => {
       this.moyenne=data;
     });
+    this.http.get<number>("http://localhost:8080/notelaplusbasse").subscribe(data => {
+      this.notelaplusbasse=data;
+    });
+    this.http.get<number>("http://localhost:8080/notelaplushaute").subscribe(data => {
+      this.notelaplushaute=data;
+    });
+    this.http.get<number>("http://localhost:8080/nombreetu").subscribe(data => {
+      this.nombreetu=data;
+    });
+    this.http.get<number>("http://localhost:8080/nombrenseignants").subscribe(data => {
+      this.nombrenseignants=data;
+    });
+    this.http.get<number>("http://localhost:8080/nombrepostulants").subscribe(data => {
+      this.nombrepostulants=data;
+    });
+    this.http.get<number>("http://localhost:8080/jeanmich").subscribe(data => {
+      this.jeanmich=data;
+    });
+    this.http.get<number>("http://localhost:8080/totalmessages").subscribe(data => {
+      this.totalmessages=data;
+    });
   }
-
-
-
 }
