32e81b8867885d116d3d3a9978d4027bcb976f9a
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-13 16:10:19 +0200

    FINAL examen+ajout composant statistiques

--- src/app/Statistiques/statistiques.component.ts
+++ src/app/Statistiques/statistiques.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { StatistiquesService } from './service/statistiques.service';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Statistiques } from '../model/statistiques.model';
 
 @Component({
   selector: 'app-statistiques',
   templateUrl: './statistiques.component.html',
   styleUrls: ['./statistiques.component.css']
 })
-export class StatistiquesComponent {
+export class StatistiquesComponent implements OnInit {
 
-  
+  moyenne!:number;
+
+  constructor(private statistiquesService: StatistiquesService, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<number>("http://localhost:8080/moyenne_notes").subscribe(data => {
+      this.moyenne=data;
+    });
+  }
+
+
+
 }
