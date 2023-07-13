3491401d4022770717e157fdc2115ccc8d003c5f
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-13 10:59:45 +0200

    finalisation copiecomposant, css en cours

--- src/app/model/copie.model.ts
+++ src/app/model/copie.model.ts
@@ -1,3 +1,12 @@
+import { copieCompositeKey } from "./copieCompositeKey.model";
+
 export class Copie {
-    
+
+    idCopie:copieCompositeKey;
+    note: number;
+
+    constructor(idCopie:copieCompositeKey, note:number) {
+        this.idCopie=idCopie;
+        this.note=note;
+    }
 }
